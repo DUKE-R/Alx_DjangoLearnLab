0513d245d02bbb328dd4c9b90385307e2565c61b
Richard <odhiamboritchy@gmail.com>
2024-11-10 15:54:29 +0300

    task3

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -18,3 +18,12 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', views.register, name='register'),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+]
