ccad5e8dbda15089fa42602a34823bb200cac82a
Richard <odhiamboritchy@gmail.com>
2024-11-10 16:37:03 +0300

    main

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -58,8 +58,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('book/add/', views.add_book, name="add_book"),  # Add book view
-    path('book/edit/<int:pk>/', views.edit_book, name="edit_book"),  # Edit book view with primary key (pk) for identifying the book
-    path('book/delete/<int:pk>/', views.delete_book, name="delete_book"),  # Delete book view
+    path('book/add/', views.add_book, name="add_book/"),  # Add book view
+    path('book/edit/<int:pk>/', views.edit_book, name="edit_book/"),  # Edit book view with primary key (pk) for identifying the book
+    path('book/delete/<int:pk>/', views.delete_book, name="delete_book/"),  # Delete book view
 ]
 
