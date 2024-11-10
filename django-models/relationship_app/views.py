66b36f92e78d08a5da1551ecd4ba4e1e01414b3e
Richard <odhiamboritchy@gmail.com>
2024-11-10 15:58:24 +0300

    final

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -40,6 +40,9 @@ from django.contrib.auth.decorators import user_passes_test
 # Helper function to check if user is an admin
 def is_admin(user):
     return user.userprofile.role == 'Admin'
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
 
 # Helper function to check if user is a librarian
 def is_librarian(user):
