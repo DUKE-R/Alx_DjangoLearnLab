0e856f0544878d8219bac4c11ce58b4cf0811a4d
Richard <odhiamboritchy@gmail.com>
2024-11-10 15:37:33 +0300

    commit

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,3 +7,14 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),  # URL for function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # URL for class-based view
 ]
+
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+]
