739b184c91a50d178daf2cb73b8ee06573286d6e
Richard <odhiamboritchy@gmail.com>
2024-11-17 15:15:31 +0300

    form.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -9,3 +9,19 @@ class BookForm(forms.Form):
     title = forms.CharField(max_length=200)
     author = forms.CharField(max_length=100)
     published_date = forms.DateField()
+
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date']
+
+    # Optionally, you can add custom validation here if needed
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        # Example of a simple validation: ensuring title is not empty
+        if not title:
+            raise forms.ValidationError("Title is required.")
+        return title
