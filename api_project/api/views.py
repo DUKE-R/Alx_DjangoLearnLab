aa3680900e38326786e1b894e48d1b8726261f36
Richard <odhiamboritchy@gmail.com>
2024-11-24 17:52:51 +0300

    API ENDpoint

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import ListAPIView
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(ListAPIView):
+    """
+    API view to retrieve a list of all books.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
