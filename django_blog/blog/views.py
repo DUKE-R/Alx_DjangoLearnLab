a0282d95878fcb979800f85d4328692f93eb2549
Richard <odhiamboritchy@gmail.com>
2024-12-03 14:17:41 +0300

    Implementing Authentication

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after registration
+            return redirect("profile")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "blog/register.html", {"form": form})
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        user = request.user
+        user.email = request.POST.get("email", user.email)
+        user.save()
+        return render(request, "blog/profile.html", {"success": True})
+    return render(request, "blog/profile.html")
