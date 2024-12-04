33dc557cd97ab8f8ec3a3d35c51fa7c45c3c1525
Richard <odhiamboritchy@gmail.com>
2024-12-04 13:51:44 +0300

    task 2

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,6 +4,11 @@ from django.contrib.auth import login
 from .forms import CustomUserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from .models import Post
+
 
 def register(request):
     if request.method == "POST":
@@ -24,3 +29,40 @@ def profile(request):
         user.save()
         return render(request, "blog/profile.html", {"success": True})
     return render(request, "blog/profile.html")
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from blog import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
