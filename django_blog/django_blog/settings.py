aa4677496a414720bf723f7ef1e73048601d428d
Richard <odhiamboritchy@gmail.com>
2024-12-07 12:50:52 +0300

    host port

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,12 +75,19 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+from decouple import config
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  # Or 'django.db.backends.mysql'
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
+
 
 
 # Password validation
