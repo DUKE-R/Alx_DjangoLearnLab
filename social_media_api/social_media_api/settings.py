30e3036513869e64e536c435d28b1e0625961439
Richard <odhiamboritchy@gmail.com>
2024-12-14 12:54:54 +0300

    debug

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8*^(#x_r=s^kwtt(uly=j2lff%qjbdi*%gkqiq5cxt1#9ji%my'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
